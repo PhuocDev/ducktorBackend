2e10b9452d4ad6478d56692a5477ee968a9495f4
Phuoc Bui <phuocbui@kms.com.vn>
2023-01-03 22:59:48 +0700

    inf3 news completed

---
+++ src/main/java/com/example/docktor_mobile/news/News.java
@@ -0,0 +1,11 @@
+package com.example.docktor_mobile.news;
+
+import lombok.Data;
+
+@Data
+public class News {
+    private String title;
+    private String description;
+    private String url;
+    private String urlToImage;
+}
